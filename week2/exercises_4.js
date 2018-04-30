84ec31b7aef4018095ee4170ad9cc7b91bb5fd34
ajinfadhila <38447782+ajinfadhila@users.noreply.github.com>
2018-04-30 18:36:23 +0700

    update exercises_4.js
    update

--- week2/exercises_4.js
+++ week2/exercises_4.js
@@ -1,25 +1,25 @@
 var tgl = 31;
-var bln	= "";
+var bln	= 1;
 var thn = 2000;
  if (tgl >= 1 && tgl <= 31) {
  	if (bln > 0 && bln <= 12) {
 
  		switch(bln){
- 			case 1: bln 	= "January";
- 			case 2: bln 	= "February";
- 			case 3: bln 	= "Maret";
- 			case 4: bln 	= "April";
- 			case 5: bln 	= "Mei";
- 			case 6: bln 	= "Juni";
- 			case 7: bln 	= "Juli";
- 			case 8: bln 	= "Agustus";
- 			case 9: bln 	= "September";
- 			case 10: bln 	= "Oktober";
- 			case 11: bln 	= "November";
- 			case 12: bln 	= "Desember";
+ 			case 1: bln 	= "January"; break;
+ 			case 2: bln 	= "February"; break;
+ 			case 3: bln 	= "Maret";break;
+ 			case 4: bln 	= "April";break;
+ 			case 5: bln 	= "Mei";break;
+ 			case 6: bln 	= "Juni";break;
+ 			case 7: bln 	= "Juli";break;
+ 			case 8: bln 	= "Agustus";break;
+ 			case 9: bln 	= "September";break;
+ 			case 10: bln 	= "Oktober";break;
+ 			case 11: bln 	= "November";break;
+ 			case 12: bln 	= "Desember";break;
  		}
  		if(thn >= 1900 && thn <= 2200){
- 			console.log(tgl +" "+bln +" "+ thn);	
+ 			console.log(tgl +" "+bln +" "+ thn);
  		}else{
  			console.log("salah tahun")
  		}
@@ -29,5 +29,6 @@ var thn = 2000;
  }else{
  	console.log("salah tanggal");
  }
+
 
 
