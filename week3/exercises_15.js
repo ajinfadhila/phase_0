d843371605a3ade98b464bbae259e82e84dd3949
ajinfadhila <38447782+ajinfadhila@users.noreply.github.com>
2018-05-10 15:27:37 +0700

    Update exercises_15.js

--- week3/exercises_15.js
+++ week3/exercises_15.js
@@ -1,15 +1,24 @@
-function groupAnimals(animals) {
-  let a = animals;
-  let b = " ";
-  for(let c = 0; c< animals.length; c++){
-     b = b + a[c];
-     
-  }
-  return b;
-}
+function groupAnimals(studentsArr) {
+    var result=[];
+    for (var i=0; i<studentsArr.length; i++){
+        var hasil=[];
+        var awal= studentsArr[i][0];
+
+        var urutan=studentsArr[i];
+        for(var j=studentsArr.length-1; j>i; j--){
+          if(awal===studentsArr[j][0]){
+            urutan=urutan+','+studentsArr[j]
+            studentsArr.splice(j,1);
+          }
+        }
+
+        hasil.push(urutan)
+        result.push(hasil)
+    }
+      return result.sort()
+    }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
